23e1005c70de8afc4d277a6259d11f11cf03d384
Jonathan Arp <jarp@nd.edu>
2020-04-29 12:12:37 -0400

    updated cap

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,8 +9,8 @@
 server "ec2-3-225-20-143.compute-1.amazonaws.com", user: "deploy" , roles: %w(web app)
 
 set :rails_env, "production"
-set :user, "deploy"
-set :use_sudo, false
+set :user, "ubuntu"
+set :use_sudo, true
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.4.6'
