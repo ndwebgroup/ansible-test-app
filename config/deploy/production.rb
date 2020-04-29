7405093cf64327dbb2ba8f9fbd987b08411b92f1
Jonathan Arp <jarp@nd.edu>
2020-04-29 13:34:11 -0400

    update bundle

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@ set :use_sudo, false
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.4.6'
 
-set :bundle_flags, "--binstubs"
+# set :bundle_flags, "--binstubs"
 
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
